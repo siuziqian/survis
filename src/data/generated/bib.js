094206c0dd6d1496761ad50442110a3d89dede4e
siuziqian <ziqia8214@gmail.com>
2023-05-14 16:30:20 +0100

    survis

---
+++ src/data/generated/bib.js
@@ -0,0 +1,114 @@
+﻿define({ entries : {
+    "da2017quantum": {
+        "author": "da Silveira, Luciano Reis and Tanscheit, Ricardo and Vellasco, Marley MBR",
+        "journal": "Expert Systems with Applications",
+        "keywords": "type: Quantum inspired evolutionary,algorithmOrdering,optimization,problemQuantum,bitVehicle,routing problem",
+        "pages": "71--83",
+        "publisher": "Elsevier",
+        "title": "Quantum inspired evolutionary algorithm for ordering problems",
+        "type": "article",
+        "volume": "67",
+        "year": "2017"
+    },
+    "han2002quantum": {
+        "author": "Han, Kuk-Hyun and Kim, Jong-Hwan",
+        "journal": "IEEE transactions on evolutionary computation",
+        "keywords": "type:Evolutionary computation,Quantum computing,Quantum mechanics,Evolution (biology),Genetic algorithms,Optimization methods,Genetic programming,Space exploration,Interference,Biology computing",
+        "number": "6",
+        "pages": "580--593",
+        "publisher": "IEEE",
+        "title": "Quantum-inspired evolutionary algorithm for a class of combinatorial optimization",
+        "type": "article",
+        "volume": "6",
+        "year": "2002"
+    },
+    "han2004quantum": {
+        "author": "Han, Kuk-Hyun and Kim, Jong-Hwan",
+        "journal": "IEEE transactions on evolutionary computation",
+        "keywords": "type:Evolutionary computation,Quantum computing,Quantum mechanics,Genetic algorithms,Convergence,Robustness,Optimization methods,Space exploration,Associate members,Evolution (biology)",
+        "number": "2",
+        "pages": "156--169",
+        "publisher": "IEEE",
+        "title": "Quantum-inspired evolutionary algorithms with a new termination criterion, H/sub/spl epsi//gate, and two-phase scheme",
+        "type": "article",
+        "volume": "8",
+        "year": "2004"
+    },
+    "nezamabadi2015quantum": {
+        "author": "Nezamabadi-Pour, Hossein",
+        "journal": "Engineering Applications of Artificial Intelligence",
+        "keywords": "type:Quantum computing,Swarm intelligence,Gravitational search algorithm,Rotation Q-gate,Binary encoded problems",
+        "pages": "62--75",
+        "publisher": "Elsevier",
+        "title": "A quantum-inspired gravitational search algorithm for binary encoded optimization problems",
+        "type": "article",
+        "volume": "40",
+        "year": "2015"
+    },
+    "nowotniak2010meta": {
+        "author": "Nowotniak, Robert and Kucharski, Jacek",
+        "booktitle": "Proc. XVII Int. Conf. on Information Technology Systems",
+        "pages": "1--17",
+        "title": "Meta-optimization of quantum-inspired evolutionary algorithm",
+        "type": "inproceedings",
+        "volume": "1",
+        "year": "2010"
+    },
+    "patvardhan2015quantum": {
+        "author": "Patvardhan, Chellapilla and Bansal, Sulabh and Srivastav, Anand",
+        "journal": "Memetic Computing",
+        "keywords": "type:Quantum Inspired Evolutionary Algorithm,Combinatorial optimization,Knapsack problem",
+        "pages": "135--155",
+        "publisher": "Springer",
+        "title": "Quantum-inspired evolutionary algorithm for difficult knapsack problems",
+        "type": "article",
+        "volume": "7",
+        "year": "2015"
+    },
+    "talbi2017new": {
+        "author": "Talbi, Hichem and Draa, Amer",
+        "journal": "Applied Soft Computing",
+        "keywords": "type:uantum-inspired evolutionary algorithms,Local search,Continuous optimization,Exploration,Exploitation",
+        "pages": "765--791",
+        "publisher": "Elsevier",
+        "title": "A new real-coded quantum-inspired evolutionary algorithm for continuous optimization",
+        "type": "article",
+        "volume": "61",
+        "year": "2017"
+    },
+    "vlachogiannis2008quantum": {
+        "author": "Vlachogiannis, John G and Lee, Kwang Y",
+        "journal": "IEEE transactions on power systems",
+        "keywords": "type:Evolutionary computation,Reactive power,Quantum computing,Power systems,Optimal control,Voltage control,Cost function,System testing,Simulated annealing,Propagation losses",
+        "number": "4",
+        "pages": "1627--1636",
+        "publisher": "IEEE",
+        "title": "Quantum-inspired evolutionary algorithm for real and reactive power dispatch",
+        "type": "article",
+        "volume": "23",
+        "year": "2008"
+    },
+    "xiong2018quantum": {
+        "author": "Xiong, Hegen and Wu, Zhiyuan and Fan, Huali and Li, Gongfa and Jiang, Guozhang",
+        "journal": "Swarm and Evolutionary Computation",
+        "keywords": "type:Quantum-inspired,evolutionary,algorithmQuantum,rotation gateFunction,optimization,problemKnapsack problem",
+        "pages": "43--57",
+        "publisher": "Elsevier",
+        "title": "Quantum rotation gate in quantum-inspired evolutionary algorithm: A review, analysis and comparison study",
+        "type": "article",
+        "volume": "42",
+        "year": "2018"
+    },
+    "zhang2011quantum": {
+        "author": "Zhang, Gexiang",
+        "journal": "Journal of Heuristics",
+        "keywords": "type:Quantum-inspired evolutionary algorithm, Evolutionary,computation, Quantum computing, Optimization",
+        "number": "3",
+        "pages": "303--351",
+        "publisher": "Springer",
+        "title": "Quantum-inspired evolutionary algorithms: a survey and empirical study",
+        "type": "article",
+        "volume": "17",
+        "year": "2011"
+    }
+}});
